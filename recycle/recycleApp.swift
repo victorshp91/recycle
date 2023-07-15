c7200453f504d2027b99a1f192173e80d80dfc0b
Victor Saint Hilaire <victorshp30@gmail.com>
2023-07-15 11:40:06 -0400

    Initial Commit

---
+++ recycle/recycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  recycleApp.swift
+//  recycle
+//
+//  Created by Victor Saint Hilaire on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct recycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
